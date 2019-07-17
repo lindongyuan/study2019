f4a32218aae7b99ed7f445efb72392419a869e40
Daniel <lindongyuan@126.com>
2019-07-17 16:50:52 +0800

    Work stations

--- py_pj/9-4-test.py
+++ py_pj/9-4-test.py
@@ -1,8 +1,8 @@
+'''
 #test 9-4
 class Restaurant():
-    '''9-1 餐馆'''
     def __init__(self,restaurant_name,cuisine_type):
-        '''初始化属性restaurnt_name、cuisine_type'''
+        #初始化属性restaurnt_name、cuisine_type
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
         self.number_served = 0
@@ -18,7 +18,7 @@ class Restaurant():
         print(str(self.number_served) + " person went to the restaurant.")
 
     def set_number_served(self,n):
-        '''将就餐人数设置为n'''
+        #将就餐人数设置为n
         self.number_served = n
 
     def increment_number_served(self,n):
@@ -35,13 +35,13 @@ my_restaurant.served_number()
 
 my_restaurant.increment_number_served(6)
 my_restaurant.served_number()
-
+'''
 #test 9-5
 class User():
     def __init__(self,first_name,last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.login_attempts = 2
+        self.login_attempts = 9
 
     def describe_user(self):
         print("你的姓名是：" + self.first_name.title() + " " + self.last_name.title())
@@ -50,9 +50,9 @@ class User():
         print("你好！" + self.first_name.title() + " " + self.last_name.title())
 
     def increment_login_attempts(self):
-        for self.login_attempts in range(5):
-            self.login_attempts += 1
-            print(self.login_attempts)
+        for attempts in range(5):
+            attempts += self.login_attempts
+            print(attempts)
 
     def reset_login_attempts(self):
         self.login_attempts = 0
@@ -61,6 +61,5 @@ class User():
 my_user =  User('bill','clinton')
 my_user.describe_user()
 my_user.greet_user()
-
 my_user.increment_login_attempts()
 my_user.reset_login_attempts()
