45b98b2d9071a3d36498ecf727715664577f478c
Daniel <lindongyuan@126.com>
2019-07-19 17:43:35 +0800

    Work stations

--- py_pj/electric.py
+++ py_pj/electric.py
@@ -27,23 +27,34 @@ class Car():
         '''将里程表计数增加指定的量'''
         self.odometer_reading += miles
 
+class Battery():
+    '''一次模拟电动汽车电瓶的简单尝试'''
+
+    def __init__(self,battery_size=70):
+        '''初始化电瓶属性'''
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        '''打印一条描述电瓶容量的信息'''
+        print("The car has a " + str(self.battery_size) + "-kWh battery.")
+
 class ElectricCar(Car):
     '''电动车的独到之处'''
 
     def __init__(self,make,model,year):
         #初始化父类属性
         super().__init__(make,model,year)
-        self.bettery_size = 70
-
+        self.battery = Battery()
+'''
     def describe_battery(self):
-        '''打印一条描述电瓶容量的消息'''
+        #打印一条描述电瓶容量的消息
         print("This car has a " + str(self.bettery_size) + '-kWh battery.')
 
     def fill_gas_tank(self):
-        '''电动车没有油箱'''
+        #电动车没有油箱
         print("This car doesn't need a gas tank!")
-        
+'''
 
 my_tesla = ElectricCar('tesla','model s','2016')
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
