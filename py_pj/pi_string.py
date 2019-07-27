79692ef5dae6cdab09fdc77632dec1c7bd62edf1
Daniel <lindongyuan@126.com>
2019-07-27 17:36:53 +0800

    Work stations

---
+++ py_pj/pi_string.py
@@ -0,0 +1,22 @@
+filename = 'pi_digits.txt'
+filename2 = 'pi_million_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
+
+with open(filename2) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string[:52] + "...")
+print(len(pi_string))
